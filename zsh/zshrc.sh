a3919555f7455725800ddc36f78e7a799f94ccc7
dashedstripes <adam@pateo.co>
2021-04-30 16:28:56 -0500

    clean up zshrc

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -7,21 +7,12 @@ source ~/.bash_profile
 
 COMPOSER_MEMORY_LIMIT=-1
 
-alias minecraft='gcloud beta compute ssh --zone "us-east1-b" "minecraft-server" --project "pateo-304720"'
 alias c=clear
 alias r="source ~/.zshrc"
 alias home=~/
-alias des=~/Desktop
-alias dow=~/Downloads
-alias doc=~/Documents
-alias one=~/OneDrive
-alias cod=~/code
-alias dot=~/.dotfiles
-alias fav=~/code/favish
-alias per=~/code/personal
 
 alias drush="docker-compose exec php drush"
-alias kgcc="kubectl get pods  | grep cloud-command | cut -d' ' -f1"
+alias kgcc="kubectl get pods | grep cloud-command | cut -d' ' -f1"
 alias klf="kubectl logs -f --tail=10"
 
 alias compi='composer install --ignore-platform-reqs'
@@ -30,44 +21,11 @@ alias compu='composer update --ignore-platform-reqs'
 
 alias yr="yarn --cwd next run lint"
 
-# Hook into cd, show all files on cd.
-chpwd() {
-  ls
-}
+unalias gr
+unalias gf
 
-# Google Search
-gs() {
-  open "https://www.google.com/search?q=$1"
-}
-
-# Youtube Search
-you() {
-  open "https://www.youtube.com/results?search_query=$1"
-}
-
-# Generators
-
-express-website() {
-  if [[ -z $1 ]] then
-    echo "usage: express-website project-name"
-  else
-    sh ~/.dotfiles/scripts/express-website.sh $1
-  fi
-}
-
-# Docker
-
-docker-stop-all() {
-  docker stop $(docker ps -aq)
-}
-
-docker-remove-containers() {
-  docker stop $(docker ps -aq)
-  docker rm $(docker ps -aq)
-}
-
-docker-remove-images() {
-  docker rmi $(docker images -q)
+gr() {
+  git commit -m "refactor(${1}): ${2}";
 }
 
 # Display current kubectl context https://github.com/superbrothers/zsh-kubectl-prompt
