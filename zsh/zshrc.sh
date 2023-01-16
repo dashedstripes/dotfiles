80eea9f54424f0c2ef8433d885a55fc72fcbb049
Adam Gray <adamgrayscale@gmail.com>
2023-01-16 16:25:44 -0600

    updates

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -4,4 +4,6 @@ export GIT_EDITOR=vim
 ZSH_THEME="robbyrussell"
 plugins=(git vi-mode docker docker-compose)
 
+alias o2h="cd /Users/adam/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Personal/.obsidian/plugins/obsidian-to-hugo/"
+
 source $ZSH/oh-my-zsh.sh
